c1c6e7cb685436ce5250e18e95ea92a3aeb7527c
xrvili <xurelxurel100@gmail.com>
2025-05-05 18:09:57 -0400

    Create autoupdate.lua

---
+++ Modules/autoupdate.lua
@@ -0,0 +1,9 @@
+return {
+  ["Hoops Life"] = "v1.0.0",
+  ["ReaHListic"] = "v1.0.0",
+  ["Hoop Nation"] = "v1.0.0",
+  ["RH2"] = "v1.0.0",
+  ["Hoop Journey"] = "v1.0.0",
+  ["Basketball Legends"] = "v1.0.0",
+  ["Universe Football"] = "v1.0.0"
+}

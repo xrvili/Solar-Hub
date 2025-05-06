9306f400281eef2ae0cba8b2f29135ae9853d649
xrvili <xurelxurel100@gmail.com>
2025-05-06 17:32:38 -0400

    Create requirements.lua

---
+++ Modules/requirements.lua
@@ -0,0 +1,11 @@
+return {
+  [2931782391] = {
+    "hookmetamethod",
+    "getnamecallmethod",
+    "checkcaller",
+    "isfunctionhooked",
+    "restorefunction",
+    "getrawmetatable",
+    {"http_request","syn.request","request","HttpPost"}
+  },
+}
